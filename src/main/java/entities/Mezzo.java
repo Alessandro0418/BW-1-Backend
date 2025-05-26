e341db178e85fa71f42705eb832d470a363df9a4
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-05-26 13:58:07 +0200

    Updated classes

--- src/main/java/entities/Mezzo.java
+++ src/main/java/entities/Mezzo.java
@@ -1,4 +1,8 @@
 package entities;
+import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Mezzo {
     private long id;
@@ -7,8 +11,45 @@ public class Mezzo {
     private StatoMezzo statoAttuale;
 
     //Periodi servizio e manutenzione
+    private List<PeriodoOperativo> periodiOperativi = new ArrayList<>();
 
     //Biglietti vidimati
+    private List<VidimazioneBiglietti> vidimazioni = new ArrayList<>();
 
     //Costruttori
+    public Mezzo(){
+
+    }
+
+    public Mezzo(int capienza, TipoMezzo tipo) {
+        this.capienza = capienza;
+        this.tipo = tipo;
+        this.statoAttuale = StatoMezzo.IN_SERVIZIO;
+    }
+
+    //Getter e setter
+
+    public long getId() {
+        return id;
+    }
+
+    public int getCapienza() {
+        return capienza;
+    }
+
+    public StatoMezzo getStatoAttuale() {
+        return statoAttuale;
+    }
+
+    public List<PeriodoOperativo> getPeriodiOperativi() {
+        return periodiOperativi;
+    }
+
+    public List<VidimazioneBiglietti> getVidimazioni() {
+        return vidimazioni;
+    }
+
+    public TipoMezzo getTipo() {
+        return tipo;
+    }
 }
