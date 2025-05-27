ba99a9d89682745ecc3e1dba8de21fe0d44a7385
Alessandro0418 <aledigiannangonio@gmail.com>
2025-05-27 17:29:08 +0200

    Main.java + Admin.java

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,26 @@
 package main;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.Scanner;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("postgres");
+
+    EntityManager entityManager = emf.createEntityManager();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Seleziona un opzione:");
+        System.out.println("1. Login");
+        System.out.println("2. Registrati");
+
+
+    }
 }
