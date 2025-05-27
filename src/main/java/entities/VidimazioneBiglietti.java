a075450cedaeccbfc155a157c96f0ab90445d07e
ChosenUndeaad <32023627+ChosenUndeaad@users.noreply.github.com>
2025-05-27 11:03:40 +0200

    Update VidimazioneBiglietti.java

--- src/main/java/entities/VidimazioneBiglietti.java
+++ src/main/java/entities/VidimazioneBiglietti.java
@@ -1,4 +1,63 @@
 package entities;
 
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class VidimazioneBiglietti {
+
+    @Id
+    @GeneratedValue ( strategy = GenerationType.IDENTITY)
+    private long id;
+
+    //Vidimato
+    @ManyToOne
+    @JoinColumn(name = "biglietto_id", nullable = false)
+    private Biglietto biglietto;
+
+    //Mezzo
+    @ManyToOne
+    @JoinColumn(name = "mezzo_id", nullable = false)
+    private Mezzo mezzo;
+
+    private LocalDateTime dataOraVidimazione;
+
+    //Costruttori
+    public VidimazioneBiglietti(){}
+
+    public VidimazioneBiglietti(Biglietto biglietto, Mezzo mezzo, LocalDateTime dataOraVidimazione) {
+        this.biglietto = biglietto;
+        this.mezzo = mezzo;
+        this.dataOraVidimazione = dataOraVidimazione;
+    }
+
+    //Get e set
+
+    public long getId() {
+        return id;
+    }
+
+    public Biglietto getBiglietto() {
+        return biglietto;
+    }
+
+    public void setBiglietto(Biglietto biglietto) {
+        this.biglietto = biglietto;
+    }
+
+    public Mezzo getMezzo() {
+        return mezzo;
+    }
+
+    public void setMezzo(Mezzo mezzo) {
+        this.mezzo = mezzo;
+    }
+
+    public LocalDateTime getDataOraVidimazione() {
+        return dataOraVidimazione;
+    }
+
+    public void setDataOraVidimazione(LocalDateTime dataOraVidimazione) {
+        this.dataOraVidimazione = dataOraVidimazione;
+    }
 }
