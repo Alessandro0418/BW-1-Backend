85f90077e305cf43d1170d1219b448a3032e5caf
Alessandro0418 <aledigiannangonio@gmail.com>
2025-05-30 11:20:36 +0200

    commit

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,10 @@
 package main;
 
 import Enumeration.TipoAbbonamento;
+import Enumeration.TipoMezzo;
+import dao.BigliettoDao;
+import dao.MezzoDao;
+import dao.TesseraDao;
 import entities.*;
 import service.*;
 import service.PuntoEmissioneService;
@@ -19,13 +23,29 @@ public class Main {
     private static final EntityManager em = emf.createEntityManager();
     private static final Scanner scanner = new Scanner(System.in);
     private static final GestioneUtentiService servizio = new GestioneUtentiService(em);
+    private static TesseraDao tesseraDao;
+    private static BigliettoDao bigliettoDao;
+    private static MezzoDao mezzoDao;
+
 
 
 
     public static void main(String[] args) {
         PuntoEmissioneService pes = new PuntoEmissioneService(em);
         pes.inizializzaPuntiEmissione();
+        tesseraDao = new TesseraDao(em);
+        bigliettoDao = new BigliettoDao(em);
+        mezzoDao = new MezzoDao(em);
 
+        MezzoDao mezzoDao = new MezzoDao(em);
+
+        Mezzo tram = new Mezzo();
+        tram.setTipo(TipoMezzo.TRAM);
+        mezzoDao.salva(tram);
+
+        Mezzo autobus = new Mezzo();
+        autobus.setTipo(TipoMezzo.AUTOBUS);
+        mezzoDao.salva(autobus);
 
         while (true) {
             System.out.println("Benvenuto!");
@@ -126,86 +146,92 @@ public class Main {
         if (utente.getTipoUtente().name().equals("ADMIN")) {
             System.out.println("MENU ADMIN");
 
-            while (true) {
-                System.out.println("\n--- MENU ADMIN ---");
-                System.out.println("1. Visualizza biglietti/abbonamenti emessi per punto di emissione");
-                System.out.println("2. Visualizza biglietti/abbonamenti emessi in un periodo");
-                System.out.println("3. Visualizza biglietti vidimati su un mezzo o in un periodo");
-                System.out.println("4. Calcola tempo medio di percorrenza di una tratta da parte di un mezzo");
-                System.out.println("5. Visualizza mezzi in manutenzione o in servizio");
-                System.out.println("6. Visualizza tratte e percorrenze");
-                System.out.println("0. Esci");
-                System.out.print("Scelta: ");
-                String sceltaAdmin = scanner.nextLine();
+            if (utente.getTipoUtente().name().equals("ADMIN")) {
+                System.out.println("MENU ADMIN");
 
-                AdminService adminService = new AdminService(em);
+                while (true) {
+                    System.out.println("\n--- MENU ADMIN ---");
+                    System.out.println("1. Visualizza biglietti/abbonamenti emessi per punto di emissione");
+                    System.out.println("2. Visualizza biglietti/abbonamenti emessi in un periodo");
+                    System.out.println("3. Visualizza biglietti vidimati su un mezzo o in un periodo");
+                    System.out.println("4. Calcola tempo medio di percorrenza di una tratta da parte di un mezzo");
+                    System.out.println("5. Visualizza mezzi in manutenzione o in servizio");
+                    System.out.println("6. Visualizza tratte e percorrenze");
+                    System.out.println("0. Esci");
+                    System.out.print("Scelta: ");
+                    String sceltaAdmin = scanner.nextLine();
 
-                switch (sceltaAdmin) {
-                    case "1":
-                        List<Biglietto> biglietti = adminService.getBigliettiPerPuntoEmissione(puntoEmissioneScelto);
-                        List<Abbonamento> abbonamenti = adminService.getAbbonamentiPerPuntoEmissione(puntoEmissioneScelto);
-                        System.out.println("Biglietti emessi:");
-                        biglietti.forEach(System.out::println);
-                        System.out.println("Abbonamenti emessi:");
-                        abbonamenti.forEach(System.out::println);
-                        break;
+                    AdminService adminService = new AdminService(em);
 
-                    case "2":
-                        System.out.print("\nRicerca: \nInserisci data inizio (yyyy-mm-dd): ");
-                        LocalDate inizio = LocalDate.parse(scanner.nextLine());
-                        System.out.print("Inserisci data fine (yyyy-mm-dd): ");
-                        LocalDate fine = LocalDate.parse(scanner.nextLine());
-                        List<Biglietto> bigliettiPeriodo = adminService.getBigliettiInPeriodo(inizio, fine);
-                        List<Abbonamento> abbonamentiPeriodo = adminService.getAbbonamentiInPeriodo(inizio, fine);
-                        System.out.println("Biglietti emessi nel periodo:");
-                        bigliettiPeriodo.forEach(System.out::println);
-                        System.out.println("Abbonamenti emessi nel periodo:");
-                        abbonamentiPeriodo.forEach(System.out::println);
-                        break;
+                    switch (sceltaAdmin) {
+                        case "1":
+                            List<Biglietto> biglietti = adminService.getBigliettiPerPuntoEmissione(puntoEmissioneScelto);
+                            List<Abbonamento> abbonamenti = adminService.getAbbonamentiPerPuntoEmissione(puntoEmissioneScelto);
+                            System.out.println("Biglietti emessi:");
+                            biglietti.forEach(System.out::println);
+                            System.out.println("Abbonamenti emessi:");
+                            abbonamenti.forEach(System.out::println);
+                            break;
 
-                    case "3":
-                        System.out.print("Inserisci ID del mezzo: ");
-                        Long mezzoId = Long.parseLong(scanner.nextLine());
-                        Mezzo mezzo = em.find(Mezzo.class, mezzoId);
-                        if (mezzo != null) {
-                            List<Biglietto> bigliettiVidimati = adminService.getBigliettiVidimatiPerMezzo(mezzo);
-                            bigliettiVidimati.forEach(System.out::println);
-                        } else {
-                            System.out.println("Mezzo non trovato.");
-                        }
-                        break;
+                        case "2":
+                            System.out.print("\nRicerca: \nInserisci data inizio (yyyy-mm-dd): ");
+                            LocalDate inizio = LocalDate.parse(scanner.nextLine());
+                            System.out.print("Inserisci data fine (yyyy-mm-dd): ");
+                            LocalDate fine = LocalDate.parse(scanner.nextLine());
+                            List<Biglietto> bigliettiPeriodo = adminService.getBigliettiInPeriodo(inizio, fine);
+                            List<Abbonamento> abbonamentiPeriodo = adminService.getAbbonamentiInPeriodo(inizio, fine);
+                            System.out.println("Biglietti emessi nel periodo:");
+                            bigliettiPeriodo.forEach(System.out::println);
+                            System.out.println("Abbonamenti emessi nel periodo:");
+                            abbonamentiPeriodo.forEach(System.out::println);
+                            break;
 
-                    case "4":
-                        System.out.print("Inserisci ID tratta: ");
-                        Long trattaId = Long.parseLong(scanner.nextLine());
-                        Tratta tratta = em.find(Tratta.class, trattaId);
-                        if (tratta != null) {
-                            double tempoMedio = adminService.calcolaTempoMedioTratta(tratta);
-                            System.out.println("Tempo medio di percorrenza: " + tempoMedio + " minuti");
-                        } else {
-                            System.out.println("Tratta non trovata.");
-                        }
-                        break;
+                        case "3":
+                            System.out.print("Inserisci ID del mezzo: ");
+                            Long mezzoId = Long.parseLong(scanner.nextLine());
+                            Mezzo mezzo = em.find(Mezzo.class, mezzoId);
+                            if (mezzo != null) {
+                                List<Biglietto> bigliettiVidimati = adminService.getBigliettiVidimatiPerMezzo(mezzo);
+                                bigliettiVidimati.forEach(System.out::println);
+                            } else {
+                                System.out.println("Mezzo non trovato.");
+                            }
+                            break;
 
-                    case "5":
-                        List<Mezzo> inManutenzione = adminService.getMezziInManutenzione();
-                        List<Mezzo> inServizio = adminService.getMezziInServizio();
-                        System.out.println("Mezzi in manutenzione:");
-                        inManutenzione.forEach(System.out::println);
-                        System.out.println("Mezzi in servizio:");
-                        inServizio.forEach(System.out::println);
-                        break;
+                        case "4":
+                            System.out.print("Inserisci ID tratta: ");
+                            Long trattaId = Long.parseLong(scanner.nextLine());
+                            Tratta tratta = em.find(Tratta.class, trattaId);
+                            if (tratta != null) {
+                                double tempoMedio = adminService.calcolaTempoMedioTratta(tratta);
+                                System.out.println("Tempo medio di percorrenza: " + tempoMedio + " minuti");
+                            } else {
+                                System.out.println("Tratta non trovata.");
+                            }
+                            break;
 
-                    case "6":
-                        List<Tratta> tratte = adminService.getTutteLeTratte();
-                        for (Tratta t : tratte) {
-                            System.out.println("Tratta: " + t);
-                            List<TrattaPercorsa> percorrenze = adminService.getPercorrenzeTratta(t);
-                            percorrenze.forEach(System.out::println);
-                        }
-                        break;
+                        case "5":
+                            List<Mezzo> inManutenzione = adminService.getMezziInManutenzione();
+                            List<Mezzo> inServizio = adminService.getMezziInServizio();
+                            System.out.println("Mezzi in manutenzione:");
+                            inManutenzione.forEach(System.out::println);
+                            System.out.println("Mezzi in servizio:");
+                            inServizio.forEach(System.out::println);
+                            break;
+
+                        case "6":
+                            List<Tratta> tratte = adminService.getTutteLeTratte();
+                            for (Tratta t : tratte) {
+                                System.out.println("Tratta: " + t);
+                                List<TrattaPercorsa> percorrenze = adminService.getPercorrenzeTratta(t);
+                                percorrenze.forEach(System.out::println);
+                            }
+                            break;
+                    }
                 }
             }
+
+
         } else {
             System.out.println("MENU UTENTE");
 
@@ -215,9 +241,11 @@ public class Main {
                 System.out.println("2. Acquista abbonamento");
                 System.out.println("3. Verifica validità abbonamento");
                 System.out.println("4. Richiedi o rinnova tessera");
+                System.out.println("5. Vidima Biglietto");
                 System.out.println("0. Esci");
                 System.out.print("Scelta: ");
                 String scelta = scanner.nextLine();
+
 
 
                 switch (scelta) {
@@ -240,7 +268,10 @@ public class Main {
                                 break;
 
                             case "2":
+
                                 Tessera tesseraScelta = utente.getTessere().get(0);
+
+
                                 System.out.println("Scegli tipo abbonamento: 1. Settimanale  2. Mensile");
                                 String tipoInput = scanner.nextLine();
                                 TipoAbbonamento tipoScelto;
@@ -253,6 +284,8 @@ public class Main {
                                     System.out.println("Tipo abbonamento non valido.");
                                     break;
                                 }
+
+
                                 puntoEmissioneService = new PuntoEmissioneService(em);
                                 Abbonamento abbonamento = puntoEmissioneService.emettiAbbonamento(puntoEmissioneScelto, tesseraScelta, tipoScelto);
                                 System.out.println("Abbonamento emesso con successo! ID: " + abbonamento.getId());
@@ -263,10 +296,77 @@ public class Main {
                         }
                         break;
 
+                    case "3":
+                        List<Abbonamento> abbonamenti = utente.getTessere().stream()
+                                .flatMap(t -> t.getAbbonamenti().stream())
+                                .toList();
+
+                        if (abbonamenti.isEmpty()) {
+                            System.out.println("Nessun abbonamento trovato.");
+                            break;
+                        }
+
+                        for (Abbonamento ab : abbonamenti) {
+                            boolean valido = ab.isValido();
+                            System.out.println("Abbonamento ID: " + ab.getId() + " - Tipo: " + ab.getTipoAbbonamento()
+                                    + " - Emesso il: " + ab.getDataInizio() + " - Valido: " + (valido ? "SÌ" : "NO"));
+                        }
+                        break;
+
+                    case "4":
+                        System.out.println("Inserisci il numero della tessera da rinnovare:");
+                        String numero = scanner.nextLine();
+                        Tessera tessera = tesseraDao.findByNumero(numero);
+                        if (tessera != null) {
+                            tesseraDao.rinnovaTessera(tessera);
+                            System.out.println("Tessera rinnovata con successo!");
+                        } else if (tessera == null){
+                            System.out.println("Tessera non trovata.");
+                        } else{
+                            System.out.println("Tessera ancora valida");
+                        }
+                        break;
+
+                    case "5":
+                        System.out.print("Inserisci ID biglietto da vidimare: ");
+                        String idInput = scanner.nextLine();
+
+                        try {
+                            long idBiglietto = Long.parseLong(idInput);
+
+                            // Mostro i mezzi disponibili con ID e tipo
+                            List<Mezzo> mezzi = mezzoDao.trovaTutti();
+                            System.out.println("Mezzi disponibili:");
+                            for (Mezzo m : mezzi) {
+                                System.out.println("ID: " + m.getId() + " - Tipo: " + m.getTipo().name());
+                            }
+
+                            System.out.print("Inserisci ID del mezzo per la vidimazione: ");
+                            long idMezzo = Long.parseLong(scanner.nextLine());
+                            Mezzo mezzo = mezzoDao.trovaPerId(idMezzo);
+
+                            if (mezzo == null) {
+                                System.out.println("Mezzo non trovato.");
+                            } else {
+                                String risultato = bigliettoDao.vidimaBiglietto(idBiglietto, mezzo);
+                                System.out.println(risultato);
+                            }
+
+                        } catch (NumberFormatException e) {
+                            System.out.println("Input non valido. Inserisci un numero valido.");
+                        }
+                        break;
+
+                    case "0":
+                        System.out.println("Uscita dal menu utente.");
+                        return; // esci dal menu utente (puoi anche fare break se vuoi far tornare al menu principale)
+
                     default:
                         System.out.println("Scelta non valida.");
                 }
+
             }
+
         }
     }
 }
